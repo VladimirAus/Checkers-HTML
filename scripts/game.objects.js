69522a4467c2df4a990944f428a9e9dee1d90c8a
VladimirAus <v_roudakov@yahoo.com>
2012-07-23 01:50:33 +1000

    Object initialisation. Move and Position objects added

--- scripts/game.objects.js
+++ scripts/game.objects.js
@@ -1,18 +1,42 @@
 // Singleton objects
 
-// Checker object
-var Game = function() {
+// CheckersGame object
+var CheckersGame = function() {
+	this.players = [new Player(),new Player()];
+	this.currentPlayerIndex = 0;
+	this.board = new Board();
 }
 
+// Board object
 var Board = function() {
+	this.squares = [];
 }
 
-// Singleton objects
+// Other objects
 
 // Player object
 function Player() {
+	this.name = "";
+	this.color = "";
+	this.checkers = [];
 }
 
 // Checker object
 function Checker() {
+	this.currentPosition = new Position();
+	this.kinged = false;
+	this.captured = false;
+}
+
+// Move object: state
+function Move() {
+	this.playerColor = "";
+	this.startPosition = new Position();
+	this.endPosition = new Position();
+}
+
+// Position object
+function Position() {
+	this.x = "";
+	this.y = "";
 }
