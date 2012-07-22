e31544a8a88f4e4d3df34ae205e24bff988f7643
VladimirAus <v_roudakov@yahoo.com>
2012-07-22 22:39:01 +1000

    Initial objects

---
+++ scripts/game.objects.js
@@ -0,0 +1,18 @@
+// Singleton objects
+
+// Checker object
+var Game = function() {
+}
+
+var Board = function() {
+}
+
+// Singleton objects
+
+// Player object
+function Player() {
+}
+
+// Checker object
+function Checker() {
+}
